6cc04dc0bb6376dcd1b25fc333e4c82761527dba
John Cowie <john.a.cowie@gmail.com>
2013-07-02 18:54:04 +0100

    Updated startup and shutdown scripts

--- shutdown.py
+++ shutdown.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 import subprocess, sys
+
+PROCESS_NAME = sys.argv[1]
+
+pidFileName = "%s.pid" %(PROCESS_NAME)
+pidFile = open(pidFileName)
+
+pid = pidFile.readline()
+pidFile.close()
+
+print "Killing process ", pid
+subprocess.Popen('rm %s && kill %s' %(pidFileName, pid), shell=True)
+
--- startup.py
+++ startup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import subprocess, sys
 
-JAR_FILE = sys.argv[1]
-CONFIG_FILE = sys.argv[2]
-LOG_FILE = sys.argv[3]
+PROCESS_NAME = sys.argv[1]
+JAR_FILE = sys.argv[2]
+CONFIG_FILE = sys.argv[3]
+LOG_FILE = sys.argv[4]
 
 command = 'nohup java -jar -server %s %s' %(JAR_FILE, CONFIG_FILE)
 
 logFile = open(LOG_FILE, 'w+')
-pidFile = open("process.pid", 'w+')
+pidFile = open("%s.pid" %(PROCESS_NAME), 'w+')
 
+# TODO add logic to only run startup command if pid doesn't exist?
 p = subprocess.Popen(command, shell=True, stdout=logFile, stderr=logFile)
 
 print "Started ", p.pid
